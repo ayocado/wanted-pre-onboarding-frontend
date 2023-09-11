6ebece36f441a76d4a90cb87355b9ee70977cce4
ayocado <yoonbean4413@naver.com>
2023-09-11 12:58:42 +0900

    update : 회원가입과 로그인 구현 및 유효성 검사기능 추가

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,44 @@
 import React from "react";
+import { useInputValidation } from "../utils/useInputValidation";
 
 const Signup = (props) => {
+  const emailValidation = (email) => email.includes("@");
+  const passwordValidation = (password) => password.length >= 8;
+
+  const emailInput = useInputValidation("", emailValidation);
+  const passwordInput = useInputValidation("", passwordValidation);
+
   return (
-    <>
-      <h3>회원가입 페이지</h3>
-    </>
+    <div>
+      <form>
+        <p>
+          <label for="email">이메일</label>
+          <input
+            id="email"
+            data-testid="email-input"
+            value={emailInput.value}
+            placeholder="@를 포함하여 입력해주세요"
+            onChange={(e) => emailInput.handleChange(e.target.value)}
+          />
+        </p>
+        <p>
+          <label for="password">비밀번호</label>
+          <input
+            id="password"
+            data-testid="password-input"
+            value={passwordInput.value}
+            placeholder="8자 이상 입력해주세요"
+            onChange={(e) => passwordInput.handleChange(e.target.value)}
+          />
+        </p>
+        <button
+          data-testid="signup-button"
+          disabled={!emailInput.isValid || !passwordInput.isValid}
+        >
+          회원가입
+        </button>
+      </form>
+    </div>
   );
 };
 
